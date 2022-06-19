990de519281bca080c2a01ae7aa19a9649903de0
lmuhamadaslan <lmuhamadaslan@gmail.com>
2022-06-19 16:36:41 +0800

    fix error

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ import AkmTestResultRoute from "./routes/AkmTestResultRoute.js";
 
 const app = express();
 dotenv.config();
+const port = process.env.PORT || 5000;
 
 // connect to mongodb
 mongoose.connect("mongodb://localhost:27017/akm_database", {
@@ -56,6 +57,6 @@ app.use((err, req, res, next) => {
 });
 
 // listener
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 5000");
 });
